f749d3c7040ccf94d1b43c2669bde0ac6c9595a1
Eric <eqhayes@protonmail.com>
2024-07-08 17:56:16 -0400

    login logout

--- backend/scripts/make_db.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-HERE="$(cd "$(dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
-DB_FILE=$HERE/../db.db
-SCHEMA=$HERE/schema.sql
-SEED=$HERE/seed.sql
-sqlite3 $DB_FILE < $SCHEMA
-sqlite3 $DB_FILE < $SEED
-
-
